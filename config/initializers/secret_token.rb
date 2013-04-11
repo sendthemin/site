3e647954e024e0f37808cd64d07d60b017e09938
Tamashua <web@tamashua.com>
2013-04-10 21:16:41 -0400

    inital push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sendthemin::Application.config.secret_token = 'd01ed2db684b44d5e42a6615402f692d35ac216e2841a0539158c8af5e1bfb099dfae7eff72017580f864c6d85cd8da62a22770e7c67c308f053da8810bd037e'
