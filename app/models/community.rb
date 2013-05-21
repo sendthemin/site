cb63f85faad432e55ec348e5b6c5dfd9545b09e6
sendthemin <roten66@gmail.com>
2013-05-21 02:03:37 -0400

    added images to logged in home

--- app/models/community.rb
+++ app/models/community.rb
@@ -11,6 +11,6 @@ validates_attachment :image, presence: true,
                             content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
                             size: { less_than: 5.megabytes }
   belongs_to :community
-  has_attached_file :image, styles: { medium: "320x240>"}
+  has_attached_file :image, styles: { medium: "240x320>", thumb: "60x80" }
 
 end
